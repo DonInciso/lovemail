27a2f1aa0717e7c66c8a8ca2cf24ef1a186d4e6d
Don Inciso <esmeraldo_inciso@umindanao.edu.ph>
2020-02-16 03:00:07 +0800

    Update connection.php to connect to online database

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
 <div class="row">
 	<div class="jumbotron" style="text-align:  center;">
- 		Copyright HAPPY SAD MI SOLUTIONS© 2020. <br>
+ 		©2020 HAPPY SAD MI SOLUTIONS <br>
  		<span class="fa fa-phone"></span> +639469661348 <br>
  		<span class="fa fa-envelope"></span> esmeraldo_inciso@umindanao.edu.ph <br>
  		<span class="fa fa-facebook"></span> Don Retardo Inciso <br>
