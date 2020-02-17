e1d3054129fda2b945732fc673837af95999cc62
Don Inciso <esmeraldo_inciso@umindanao.edu.ph>
2020-02-17 21:32:43 +0800

    Update Connection

--- executes/saveuser.php
+++ executes/saveuser.php
@@ -6,7 +6,7 @@
    $email=$_POST['email'];
    $password1=$_POST['password1'];
    $password2=$_POST['password2'];
-   
+   $same = 'false';
   
    session_start();
       if($password1<>$password2){
@@ -19,7 +19,11 @@
         $sql_result = mysqli_query($connection,$sqli);
         while($row=mysqli_fetch_assoc($sql_result))
         {
-          if ($email != $row['email']){
+          if ($email == $row['email']){
+            $same = 'true';
+          }
+        }
+          if ($same == 'false'){
             $sql="INSERT INTO users (last_name,first_name,middle_name,bdate,email,password, date_created) VALUES ('$last_name','$first_name','$middle_name','$bdate','$email',md5('$password1'), CURRENT_TIME)";
             if(!mysqli_query($connection,$sql)){
               die("error" . mysqli_error($connection));
@@ -29,7 +33,7 @@
             $_SESSION['email_exist']="Email is already exist. Please Change.";
           }
           header("Location:../register.php");
-        }
+        
       }
 ?>
 
